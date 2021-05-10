d4d1d9a2730936ce3c597116fcedd16cf2fe7ccd
Malte Podolski <malte.podolski@web.de>
2021-05-10 11:27:08 +0200

    Add sponsor logos

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,8 +1,36 @@
-<strong>Kontakt</strong>
-<ul class="footer-list">
-  <li>webconia GmbH</li>
-  <li>Gänsemarkt 31</li>
-  <li>20354 Hamburg</li>
-  <li>+49-40-609432300</li>
-  <li>info@webconia.de</li>
-</ul>
+<section>
+  <strong>Kontakt</strong>
+  <ul class="footer-list">
+    <li>webconia GmbH</li>
+    <li>Gänsemarkt 31</li>
+    <li>20354 Hamburg</li>
+    <li>+49-40-609432300</li>
+    <li>info@webconia.de</li>
+    <li><a
+        href="https://www.webconia.de/"
+        target="_blank"
+      >www.webconia.de</a></li>
+  </ul>
+</section>
+
+
+<section>
+  <strong>Sponsoren</strong>
+  <ul class="sponsors-list">
+    <li>
+      <img src="./dist/images/logo-code-net.webp">
+    </li>
+    <li>
+      <img src="./dist/images/logo-storage-engine.webp">
+    </li>
+    <li>
+      <img src="./dist/images/logo-devloply.webp">
+    </li>
+    <li>
+      <img src="./dist/images/logo-php-burst.webp">
+    </li>
+    <li>
+      <img src="./dist/images/logo-tech-shield.webp">
+    </li>
+  </ul>
+</section>
